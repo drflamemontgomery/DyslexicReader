3c908f18b392ddb64d4044e48f77dd6375b1ca4d
drflamemontgomery <drflamemontgomery@gmail.com>
2024-07-17 10:56:05 +1200

    Moved application loop to separate function

--- src/main.zig
+++ src/main.zig
@@ -4,19 +4,26 @@ const context = @import("context.zig");
 const Context = context.Context;
 
 pub fn main() !void {
+    // Setup an ArenaAllocator so we can deallocate everything afterwards
     var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_state.deinit();
-
     const arena = arena_state.allocator();
 
+    // Initialize Our graphics context
     try Context.init();
     defer Context.terminate();
 
-    var window = try Context.new(arena, "Test", 640, 640);
+    var window = try Context.new(arena, "Dyslexic Reader", 1024, 786);
     defer window.destroy();
 
+    // Make our window collect the current events
     window.makeCurrent();
 
+    try mainLoop(arena, &window);
+}
+
+fn mainLoop(allocator: std.mem.Allocator, window: *Context) !void {
+    _ = allocator;
     while (!window.shouldClose()) {
         window.graphics.setSourceRGB(0, 0, 0);
         window.graphics.clear();
