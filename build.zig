c73268e68083ee72ea72bd027e5f788d9694be5d
drflamemontgomery <drflamemontgomery@gmail.com>
2024-07-17 11:36:11 +1200

    Removed GL and glfw3 linking for testing apis

--- build.zig
+++ build.zig
@@ -75,19 +75,18 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
+    
+    exe_unit_tests.root_module.addAnonymousImport("cairo", .{
+        .root_source_file = b.path("lib/cairo.zig"),
+    });
+
+    exe_unit_tests.linkSystemLibrary("cairo");
+    exe_unit_tests.linkLibC();
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
@@ -95,6 +94,5 @@ pub fn build(b: *std.Build) void {
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
